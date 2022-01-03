578c0ecb40d512eb3057b07f5c2829349e2dfd0d
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-01-03 20:26:25 -0300

    path.resolve

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
@@ -8,13 +9,13 @@ app.listen(PORT, ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.resolve(__dirname, '/views/login.html'));
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(path.resolve(__dirname, '/views/register.html'));
 });

fa6fadbbb798a7d4aeebaa4cbb03026f9551c23c
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-01-03 21:07:42 -0300

    modificando path

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.listen(app.get('port'), ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+    res.sendFile(path.join(__dirname, 'views/home.html'));
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '/views/login.html'));
+    res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, '/views/register.html'));
+    res.sendFile(path.join(__dirname, './views/register.html'));
 });
